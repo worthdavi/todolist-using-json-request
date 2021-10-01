edcec59e0451ce920b3bf0aaa53dba60aaace48b
davi costa <worthdavi@gmail.com>
2021-10-01 12:21:09 -0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+import TaskDetails from './components/TaskDetails';
+
+import Header from './components/Header';
+
+import AddTask from './components/AddTask';
+import Tasks from './components/Tasks';
+
+const App = () => {
+  // let message = "hello world";
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: "Ler Livros",
+      completed: true,
+    },
+  ]);
+
+  useEffect(() => {
+  const fetchTasks = async () => {
+    const { data } = await axios.get('https://jsonplaceholder.cypress.io/todos?_limit=10');
+    setTasks(data);
+  };
+  fetchTasks();
+  }, [])
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId) return {... task, completed: !task.completed};
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [... tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      complete: false,
+    }]
+    setTasks(newTasks);
+  }
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+
+
+  return (
+    <Router>
+      <div className="container">
+      <Header />
+      <Route 
+        path="/"
+        exact 
+        render={() => (
+          <>
+            <AddTask handleTaskAddition={handleTaskAddition} />
+            <Tasks
+              tasks={tasks}
+              handleTaskClick={handleTaskClick}
+              handleTaskDeletion={handleTaskDeletion} 
+            />   
+          </>
+        )}/>
+      <Route
+        path="/:taskTitle"
+        exact
+        component={TaskDetails}
+      />
+      </div>
+    </Router>  
+   
+  );
+};
+
+export default App;
